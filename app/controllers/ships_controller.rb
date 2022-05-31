751ad36329e9b255d4a894bbdd1fffd40213f87b
sam neat <samjneat@gmail.com>
2022-05-31 12:38:24 +0100

    individual ships now link to their own show page

--- app/controllers/ships_controller.rb
+++ app/controllers/ships_controller.rb
@@ -1,13 +1,13 @@
 class ShipsController < ApplicationController
-def index
-  @ships = Ship.all
-end
+  def index
+    @ships = Ship.all
+  end
 
-def new
-  @ship = Ship.new
-end
+  def new
+    @ship = Ship.new
+  end
 
-def show
-  @ship = Ship.find(params[:id])
-end
+  def show
+    @ship = Ship.find(params[:id])
+  end
 end
