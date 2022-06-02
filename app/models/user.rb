b079c5628ebe1f4af6a483a3a463884ad118f06e
sam neat <samjneat@gmail.com>
2022-06-02 10:23:36 +0100

    added validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :username, presence: true, uniqueness: true
+  validates :first_name, presence: true
+  validates :email, presence: true, uniqueness: true
 end
